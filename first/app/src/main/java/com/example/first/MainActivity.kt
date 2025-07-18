259dda9612f104abbf49d881092229e81c1faee2
Adarrak <zeniapetrov@mail.ru>
2025-07-18 16:05:20 +0200

    schooll lesson

--- first/app/src/main/java/com/example/first/MainActivity.kt
+++ first/app/src/main/java/com/example/first/MainActivity.kt
@@ -1,5 +1,28 @@
 package com.example.first
 
+class School {
+    var name: String
+    var students: ArrayList<Student>
+
+    constructor(name: String) {
+        this.name = name
+        students = arrayListOf()
+    }
+
+    fun printStudents(){
+        
+    }
+}
+
+class Student(var firstName: String, var secondName: String){
+
+}
+
+
+
 fun main(){
-    println(2+6+9-8+9)
+    println("Здравствуйте! Введите название вашей школы")
+    var schoolName: String = readln()
+    var school = School(schoolName)
+    println("Школа ${school.name} успешно создана")
 }
