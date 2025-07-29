809e022e07744d177eca3add26bf3fab3a6b140a
Adarrak <zeniapetrov@mail.ru>
2025-07-29 16:28:41 +0200

    lesson

--- firstXLM/app/src/main/java/com/example/firstxlm/MainActivity.kt
+++ firstXLM/app/src/main/java/com/example/firstxlm/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.firstxlm
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-
+        val tvQuestionWord: TextView = findViewById(R.id.tvQuestionWord)
+        tvQuestionWord.text = "asdzxcqwe"
     }
 }
