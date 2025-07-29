33fa1e1859d155222a43787c250dab4408b2752b
Евгений Протопопов <zeniapetrov@mail.ru>
2025-07-30 00:07:03 +0200

    lesson

--- first/app/src/main/java/com/example/first/MainActivity.kt
+++ first/app/src/main/java/com/example/first/MainActivity.kt
@@ -3,26 +3,37 @@ package com.example.first
 data class User(
     val name: String,
     val age: Int,
+    var isActive: Boolean = false
 )
-fun <T>transform(users: List<User>, action: (User) -> T): List<T> {
-    val result: MutableList<T> = mutableListOf()
-    for (user in users) {
-        result.add(action(user))
+/*
+fun addSymbolPostfix(hardNames: List<String>, actionAddPostfix: (String) -> User): List<User> {
+    val result: MutableList<User> = mutableListOf()
+    for (name in hardNames) {
+        result.add(actionAddPostfix.invoke("$name!"))
+    }
+    return result
+}
+*/
+
+fun IsActive(hardNames: List<User>,
+             activityCheck: (String , Int) -> User ) : List<User> {
+    val result: MutableList<User> = mutableListOf()
+    for (person in hardNames) {
+        result.add.activityCheck(person.name, person.age)
     }
 
     return result
 }
 
-fun main(){
+fun main() {
     val users: List<User> = listOf(
         User("Batraz", 24),
         User("Josef", 27),
         User("Ivan", 32)
     )
-    transform(users) {
-        println(it.name)
-        println(it.age)
-       // return@transform it.name
-    }
+
+    //val chexkUsers = IsActive(users)
+    println(users)
 }
+
 
