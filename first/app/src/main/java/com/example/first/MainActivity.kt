ad57fc36fcec79abad85aa07512a9e2f5d4bd41d
Евгений Протопопов <zeniapetrov@mail.ru>
2025-07-30 23:59:36 +0200

    lesson

--- first/app/src/main/java/com/example/first/MainActivity.kt
+++ first/app/src/main/java/com/example/first/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.first
+/*
+import android.service.credentials.Action
 
-data class User(
-    val name: String,
-    val age: Int,
-    var isActive: Boolean = false
-)
 /*
 fun addSymbolPostfix(hardNames: List<String>, actionAddPostfix: (String) -> User): List<User> {
     val result: MutableList<User> = mutableListOf()
@@ -15,15 +12,11 @@ fun addSymbolPostfix(hardNames: List<String>, actionAddPostfix: (String) -> User
 }
 */
 
-fun IsActive(hardNames: List<User>,
-             activityCheck: (String , Int) -> User ) : List<User> {
-    val result: MutableList<User> = mutableListOf()
-    for (person in hardNames) {
-        result.add.activityCheck(person.name, person.age)
-    }
-
-    return result
-}
+data class User(
+    val name: String,
+    val age: Int,
+    var isActive: Boolean = true
+)
 
 fun main() {
     val users: List<User> = listOf(
@@ -32,8 +25,70 @@ fun main() {
         User("Ivan", 32)
     )
 
-    //val chexkUsers = IsActive(users)
-    println(users)
+    val transformUser: List<User> = users.map { it }
+    transformUser.forEach { item ->
+        if (item.name.any { it in setOf('a', 'A', 'b', 'B') } || item.age >= 25) item.isActive =
+            false
+        println(item)
+    }
 }
 
 
+data class User(
+    val name: String,
+    val age: Int
+)
+
+fun main() {
+    val users = listOf(
+        User("Batraz", 24),
+        User("Josef", 27),
+        User("Nataliya", 37),
+        User("Anton", 23),
+        User("Hermione", 45),
+    )
+
+    val selectedUsers = users.filter { it.age == 24 }
+    selectedUsers.forEach {
+        println(it.name)
+    }
+}
+
+ */
+
+
+data class User(
+    val name: String,
+    var age: Int
+) {
+    val messageHandlers: MutableList<(String) -> Unit> = mutableListOf()
+
+    fun setAge(input: String) {
+        val newAge = input.toIntOrNull()
+        if (newAge != null) {
+            if (newAge in 18..99) {
+                this.age = newAge
+                messageHandlers.forEach { it.invoke("Возраст записан") }
+            } else {
+                messageHandlers.forEach { it.invoke("Возраст должен быть цифрой между 18 и 100") }
+            }
+        } else {
+            messageHandlers.forEach { it.invoke("Возраст должен быть цифрой") }
+        }
+    }
+}
+
+fun main() {
+    val user = User("Misha", 32)
+    user.messageHandlers += ::showOnConsole
+    user.messageHandlers += ::sendEmail
+    user.setAge(readln())
+}
+
+fun showOnConsole(message: String) {
+    println(message)
+}
+
+fun sendEmail(message: String) {
+    // отправка на почту
+}
